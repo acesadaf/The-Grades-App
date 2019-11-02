ef947b171b84eb00902dc0e737f1835d82c7a617
acesadaf <32542547+acesadaf@users.noreply.github.com>
2019-11-02 16:07:28 -0500

    Add files via upload

--- Main_Menu.py
+++ Main_Menu.py
@@ -7,18 +7,21 @@ class Main_Menu:
         while (done!=True):
             print("Press 1 to add a new course.")
             print("Press 2 to add an entry for an existing course.")
-            print("Press 3 to quit!")
+            print("Press 3 to view results so far!")
+            print("Press 4 to quit!")
             key="placeholder"
             while (1):
                 key = input()
-                if(key =='1' or key =='2' or key == '3'):
+                if(key =='1' or key =='2' or key == '3' or key == '4'):
                     break 
                 else:
-                    print('Enter either 1 or 2')
+                    print('Enter either 1 or 2 or 3 or 4')
             if key == '1':
                 AddCourse(username)
             elif key == '2':
                 AddEntry(username)
+            elif key == '3':
+                Results(username)
             else:
                 quit()
 
@@ -183,6 +186,68 @@ def AddEntry(username):
                     return
                 else:
                     print("Please enter either 1 or 2!")
+
+
+
+def Results(username):
+    while(1):
+        print('For what course would you like to see results?. Please enter the Course ID.')
+        print('Here are all your course IDs: ')
+        
+        conn = db.create_connection()
+        CheckString = "Select distinct(Course_ID) from Courses where username = '{}'".format(username)
+        MyCursor = conn.cursor()
+        MyCursor.execute(CheckString)
+        rows = MyCursor.fetchall()
+        for row in rows:
+            print(row[0])
+        active = True
+        while(active):
+            Course_ID = input()
+            CheckString = "Select Course_ID from Courses where username = '{}' AND Course_ID = '{}'".format(username, Course_ID)
+            MyCursor.execute(CheckString)
+            rows = MyCursor.fetchall()
+            if(len(rows)>0):
+                active = False
+            else:
+                print("This course does not exist!")
+                print("Press 1 to try again, and 2 to exit")
+                while(1):
+                    key2 = input()
+                    if key2 == '1':
+                        break
+                    if key2 == '2':
+                        return 
+                    else:
+                        print("Please press either 1 or 2!")
+        ExtractComponents = "Select Component, value, outof from Entries where username = '{}' AND Course_ID = '{}'".format(username, Course_ID)
+        MyCursor = conn.cursor()
+        MyCursor.execute(ExtractComponents)
+        rows = MyCursor.fetchall()
+        WeightedAverage = 0
+        print("Your results so far:")
+        for row in enumerate(rows):
+            print("{}: Scored: {}, Total: {}, Percentage = {}".format(row[1][0], row[1][1], row[1][2], round((row[1][1]*100/row[1][2]),2)))
+            GetWeight = "Select Weight from Courses where username = '{}' and Course_ID = '{}' and Component = '{}'".format(username, Course_ID, row[1][0])
+            MyCursor2 = conn.cursor()
+            MyCursor2.execute(GetWeight)
+            rows2 = MyCursor2.fetchall()
+            weight = rows2[0][0]
+            WeightedAverage += round((row[1][1]/row[1][2]),2) * weight
+        print("\n")
+        print("Your weighted result is {}".format(WeightedAverage))
+        print("\n")
+
+        print("Would you like to view results for any other courses?")
+        print("Press 1 to continue viewing, or 2 to go back to main menu")
+        while(1):
+            key = input()
+            if key == '1':
+                break
+            if key == '2':
+                return
+            else:
+                print("Please press either 1 or 2!")
 
 
 
